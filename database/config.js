8832ea2b31af4437522beba05ee418fef87f15af
ArleneSgo <arlenesgo15@gmail.com>
2022-03-02 09:12:41 -0700

    cambio de puerto

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('heroku_2a1b8b7153dc704','b317d6f8e4ac24','bf58bd49',{
     host: 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql',
-    port: process.env.PORT_DB
+    port: process.env.PORT_DB || 3307
 })
 //mysql://b317d6f8e4ac24:bf58bd49@us-cdbr-east-05.cleardb.net/heroku_2a1b8b7153dc704?reconnect=true
 const dbConection = async() => {
